2cb6c1f6c2de253f36f30d53f842b19e17a63592
Khayitboev Elbekjon <backenddevolpment@gmail.com>
2023-07-30 22:04:15 +0500

    talabada guruhlar uchun davomat konfrensiya va baholash funksiyalari

--- mentor/urls.py
+++ mentor/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('guruh/',GuruhlarView.as_view()),
     path('reja/',Reja.as_view()),
     path('rejadelete/<int:pk>',RejaDelete),
-    path('davomatt/<str:pk>',Davomat.as_view()),
+    path('davomatt/<str:pk>',Davomatt.as_view()),
     path('uzlashtirish/<str:pk>',Ozlashtirish.as_view()),
     path('konfensiya/',Konfrensiyaa.as_view()),
     path('konfrensiyadelete/<int:pk>',KonfrensiyaDelete),
--- mentor/views.py
+++ mentor/views.py
@@ -39,7 +39,7 @@ class IndexTeacher(View):
             }
             return render(request,'indexteacher.html',data)
 
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
 class Reja(View):
     def get(self,request):
         if request.user.is_authenticated:
@@ -47,7 +47,7 @@ class Reja(View):
                 'reja':Mavzu.objects.filter(teacher_fk__user=request.user)
             }
             return render(request, 'reja.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
     def post(self,request):
         if request.user.is_authenticated:
             Mavzu.objects.create(
@@ -57,13 +57,13 @@ class Reja(View):
                 teacher_fk=Teacher_table.objects.get(user=request.user)
             )
             return redirect('/mentor/reja/')
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
 def RejaDelete(request,pk):
     if request.user.is_authenticated:
         if Teacher_table.objects.get(user=request.user):
             Mavzu.objects.get(id=pk).delete()
             return redirect('/mentor/reja/')
-    return redirect('/mentorlogin/')
+    return redirect('/mentor/mentorlogin/')
 
 class GuruhlarView(View):
     def get(self,request):
@@ -73,39 +73,32 @@ class GuruhlarView(View):
                 'talabalar':Asosiy_talabalar_safi.objects.all()
             }
             return render(request, 'guruh.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
 
-class Davomat(View):
+class Davomatt(View):
     def get(self,request,pk):
         if request.user.is_authenticated:
             data={
                 'talabalar':Asosiy_talabalar_safi.objects.filter(guruh_fk__guruh_nom=pk),
-                'mavzu':Mavzu.objects.filter(teacher_fk__user=request.user)
-
-            }
+                'mavzu':Mavzu.objects.filter(teacher_fk__user=request.user) }
             return render(request, 'davomat.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
     def post(self, request, pk):
         if request.user.is_authenticated:
-            mavzu_id = request.POST.get('mavzu')
             talabalar = request.POST.getlist('davomat')
-            if mavzu_id and talabalar:
-                mavzu_fk = Mavzu.objects.get(id=mavzu_id),
-                guruh_fk = Guruh.objects.get(guruh_nom=pk)
-                asosiy_talabalar_safi_fk = Asosiy_talabalar_safi.objects.filter(guruh_fk__guruh_nom=pk),
-                for talaba_id in talabalar:
-                    talaba = Asosiy_talabalar_safi.objects.get(id=talaba_id)
-                    Davomat.objects.create(
-                        mavzu_fk=mavzu_fk,
-                        guruh_fk=guruh_fk,
-                        asosiy_talabalar_safi_fk=asosiy_talabalar_safi_fk,
-                        darsga_qatnashmadi=False,
-                        teacher_fk=Teacher_table.objects.get(user=request.user),
-                        talaba_fk=talaba,
-                    )
-                return redirect('/mentor/guruh/')
-
-        return redirect('/mentorlogin/')
+            mavzu_fkk = Mavzu.objects.get(id=request.POST.get('mavzu'))
+            guruh_fk = Guruh.objects.get(guruh_nom=pk)
+            for talaba_id in talabalar:
+                talaba = Asosiy_talabalar_safi.objects.get(id=talaba_id)
+                Davomat.objects.create(
+                    mavzu_fk=mavzu_fkk,
+                    guruh_fk=guruh_fk,
+                    asosiy_talabalar_safi_fk=talaba,
+                    darsga_qatnashmadi=True,
+                    teacher_fk=Teacher_table.objects.get(user=request.user),
+                )
+            return redirect('/mentor/guruh/')
+        return redirect('/mentor/mentorlogin/')
 
 
 
@@ -118,7 +111,22 @@ class Ozlashtirish(View):
 
             }
             return render(request, 'uzlashtrish.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
+    def post(self,request,pk):
+        if request.user.is_authenticated:
+            mavzu_fkk=Mavzu.objects.get(id=request.POST.get('mavzu'))
+            umumiy_ball=request.POST.get('umumiy_ball')
+            talabalar = Asosiy_talabalar_safi.objects.filter(guruh_fk__guruh_nom=pk)
+            for i in talabalar:
+                Baholash.objects.create(
+                    mavzu_fk=mavzu_fkk,
+                    guruh_fk=Guruh.objects.get(guruh_nom=pk),
+                    asosiy_talabalar_safi_fk=i,
+                    min_ball=request.POST.get(f'min_{i.id}'),
+                    max_ball=umumiy_ball
+                )
+            return redirect('/mentor/guruh/')
+        return redirect('/mentor/mentorlogin/')
 class Konfrensiyaa(View):
     def get(self,request):
         if request.user.is_authenticated:
@@ -127,7 +135,7 @@ class Konfrensiyaa(View):
                 'mavzu':Konfrensiya.objects.filter(teacher_fk__user=request.user)
             }
             return render(request, 'kanfrensa.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
     def post(self,request):
         if request.user.is_authenticated:
             Konfrensiya.objects.create(
@@ -139,13 +147,13 @@ class Konfrensiyaa(View):
                 soat=request.POST.get('soat'),
             )
             return redirect('/mentor/konfensiya/')
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
 def KonfrensiyaDelete(request,pk):
     if request.user.is_authenticated:
             if Teacher_table.objects.get(user=request.user):
                 Konfrensiya.objects.get(id=pk).delete()
                 return redirect('/mentor/konfensiya/')
-    return redirect('/mentorlogin/')
+    return redirect('/mentor/mentorlogin/')
 class BittaTalaba(View):
     def get(self,request,pk):
         if request.user.is_authenticated:
@@ -160,10 +168,10 @@ class BittaTalaba(View):
                 # 'qoldirilgan_darslar_soni':num
             }
             return render(request,'talaba_profil.html',data)
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
 class Saqlanmalar(View):
     def get(self,request):
         if request.user.is_authenticated:
 
             return render(request,'nodelist.html')
-        return redirect('/mentorlogin/')
+        return redirect('/mentor/mentorlogin/')
