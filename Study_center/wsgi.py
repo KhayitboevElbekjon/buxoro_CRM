ef4d053ab46f12c706642bc754cb5bfd44e4e53c
NishonovZarif <zarifnishonov1@gmail.com>
2025-05-22 18:40:49 +0500

    Update wsgi.py

--- Study_center/wsgi.py
+++ Study_center/wsgi.py
@@ -13,6 +13,7 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Study_center.settings')
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
